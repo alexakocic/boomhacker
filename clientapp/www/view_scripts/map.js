8d2635d9fc04efde13d706e8352701c17bbae98f
aleks <aleks@DESKTOP-JPTRD7Q>
2016-10-31 20:12:50 +0100

    dodata konekcija ka mLab bazi

--- clientapp/www/view_scripts/map.js
+++ clientapp/www/view_scripts/map.js
@@ -1,6 +1,19 @@
 ﻿var map;
 var marker;
-
+var baselayers = {
+    "Satelite": L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.satellite',
+        accessToken: 'pk.eyJ1Ijoic3VpY2lkZXNxdWFkcyIsImEiOiJjaXVyZGI4bTkwMDBrMnptc3NnMnh3cTYzIn0.bmO4d75NNaL0DHGX5xcZ6Q'
+    }),
+    "Street view": L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.mapbox-streets-v7',
+        accessToken: 'pk.eyJ1Ijoic3VpY2lkZXNxdWFkcyIsImEiOiJjaXVyZGI4bTkwMDBrMnptc3NnMnh3cTYzIn0.bmO4d75NNaL0DHGX5xcZ6Q'
+    })
+};
 var initializeMap = function (width, height, location) {
     $('#map_id').css('height', height);
     $('#map_id').css('width', width);
@@ -8,15 +21,17 @@ var initializeMap = function (width, height, location) {
 
     map = L.map('map_id').setView([43.324772, 21.895539], 13);
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    /*L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         accessToken: 'pk.eyJ1Ijoic3VpY2lkZXNxdWFkcyIsImEiOiJjaXVyZGI4bTkwMDBrMnptc3NnMnh3cTYzIn0.bmO4d75NNaL0DHGX5xcZ6Q',
         id: 'mapbox.satellite',
         maxZoom: 18
-    }).addTo(map);
-
+    }).addTo(map);*/
+    L.control.layers(baselayers, overlays).addTo(map);
+    baselayers["Satelite"].addTo(map);
     setMarker(location.latitude, location.longitude);
 }
+var overlays = {};
 
 function setMarker(latitude, longitude) {
     markerIcon = L.icon({
--- hackserver/app.js
+++ hackserver/app.js
@@ -1,5 +1,18 @@
 ﻿var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://suicidesquad:hackaton123@ds039145.mlab.com:39145/hakaton');
+
+var db = mongoose.connection;
+
+db.on('error', function () {
+    console.log("Error connecting to database")
+});
+
+db.once('open', function () {
+    console.log("Connected to the database successfully");
+});
 
 app.listen(3000, function () {
     console.log('Server started');
