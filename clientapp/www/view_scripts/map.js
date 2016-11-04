e17784728593d7d50a77a18445da59b2b947f457
Matija <Matija@DESKTOP-GJIPNIM>
2016-11-04 22:54:02 +0100

    COmmented consolelog

--- clientapp/www/view_scripts/map.js
+++ clientapp/www/view_scripts/map.js
@@ -197,7 +197,7 @@ function PointsAnime(collection) {
             var p = linePath.node().getPointAtLength(t * l);
             //Move the marker to that point
             marker.attr("transform", "translate(" + p.x + "," + p.y + ")"); //move marker
-            console.log(interpolate(t));
+            //console.log(interpolate(t));
             return interpolate(t);
         }
     } //end tweenDash
