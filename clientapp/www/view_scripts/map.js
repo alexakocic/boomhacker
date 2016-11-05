261ed5e96bdee3b9812d86f3d09f342e9599c42d
stefa <stefandj993@gmail.com>
2016-11-06 00:36:25 +0100

    dodato du;me u futeru kako bi se uvek videlo

--- clientapp/www/view_scripts/map.js
+++ clientapp/www/view_scripts/map.js
@@ -47,6 +47,7 @@ function initializeMap(width, height, location) {
     L.easyButton('glyphicon glyphicon-home', function () {
         $('#myModal').modal('show');
         $('#myModal').find('.modal-body').load("./views/settings.html");
+        $('#settings_done').click(function () { $('#myModal').modal('hide'); })
     }).addTo(map);
 }
 //[{lat:40.722390,lng:-73.995170,name:"Jovke"}]
