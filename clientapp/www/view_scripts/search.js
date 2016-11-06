ed2b98c92653b99d2001facbe0bb19f50851085f
stefa <stefandj993@gmail.com>
2016-11-06 07:17:01 +0100

    bla  blaa

--- clientapp/www/view_scripts/search.js
+++ clientapp/www/view_scripts/search.js
@@ -1,3 +1,17 @@
+﻿﻿$('#modal_done').click(function () {
+    var city = $('#search_city')[0].value;
+    
+    var url = ipadress + ":" + mainport + "/venues";
+
+    $.ajax({
+        type: "GET/",
+        data: city,
+        url: url,
+        success: function (data) {
+            updateMap(data)
+        }
+    });
+})
 ﻿$('#search_div').ready(function () {
     $('#searchBckg').css('width', $(window).width());
     $('#searchBckg').css('height', $(window).height());
